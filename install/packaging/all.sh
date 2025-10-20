5a13decc2559054407901f1878473053ce38eb28
hannes <hannes.schwanzer@gmail.com>
2025-10-20 16:59:09 +0200

    feat: install helpers

---
+++ install/packaging/all.sh
@@ -0,0 +1,5 @@
+run_logged $INSTALL_SCRIPT_LOCATION/packaging/base.sh
+run_logged $INSTALL_SCRIPT_LOCATION/packaging/aur.sh
+run_logged $INSTALL_SCRIPT_LOCATION/packaging/icons.sh
+run_logged $INSTALL_SCRIPT_LOCATION/packaging/webapps.sh
+run_logged $INSTALL_SCRIPT_LOCATION/packaging/tuis.sh
