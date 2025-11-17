d6cca92508ce3c9f61f92a351db3463922c8f25c
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2025-11-17 18:32:21 +0100

    fix: removed reference to non-existent script

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,3 +1,2 @@
 run_logged $INSTALL_SCRIPT_LOCATION/post-install/enable-mkinitcpio.sh
-run_logged $INSTALL_SCRIPT_LOCATION/post-install/pacman.sh
 source $INSTALL_SCRIPT_LOCATION/post-install/finished.sh
