cd5dc410b2d04f0b9c91d26374db91a95c154e77
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2025-11-17 18:18:11 +0100

    fix: Created wrong path for icons

--- install/packaging/icons.sh
+++ install/packaging/icons.sh
@@ -1,4 +1,4 @@
 # Copy all bundled icons to the applications/icons directory
 ICON_DIR="$HOME/.local/share/applications/icons"
 mkdir -p "$ICON_DIR"
-cp "$HYPRLAND_CONFIG_PATH/applications/icons/*.png" "$ICON_DIR/"
+cp "$HYPRLAND_CONFIG_PATH/applications/icons/"*.png "$ICON_DIR/"
